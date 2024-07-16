11b78a5da6451dc3f2a4f11e5f35c76b42d2bcdc
takabu-kouki <146901437+takabu-kouki@users.noreply.github.com>
2024-07-16 12:14:21 +0900

    02_04_確認課題

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -36,40 +36,11 @@ void GameScene::Initialize() {
 	modelBlock_ = Model::Create();
 	modelSkydome_ = Model::CreateFromOBJ("sphere", true);
 
-	textureHandel_ = TextureManager::Load("mario.jpg");
-
 	viewProjection_.Initialize();
 	worldTransform_.Initialize();
 
 	player_ = new Player();
 	player_->Initialize(model_, textureHandel_, &viewProjection_);
-
-	// 要数数
-	/*const uint32_t kNumBlockHorizontal = 20;
-	const uint32_t kNumBlockVirtical = 10;*/
-	// プロック１個分の横幅
-	/*const float kBlockWidth = 2.0f;
-	const float kBlockHeight = 2.0f;*/
-
-	//// 要数数を変更する
-	//worldTransformBlocks_.resize(kNumBlockVirtical);
-
-	//for (uint32_t i = 0; i < kNumBlockVirtical; i++) {
-	//	worldTransformBlocks_[i].resize(kNumBlockHorizontal);
-	//}
-
-	//for (uint32_t i = 0; i < kNumBlockVirtical; ++i) {
-	//	for (uint32_t j = 0; j < kNumBlockHorizontal; ++j) {
-	//		if (j % 2 == (i % 2)) {
-	//			worldTransformBlocks_[i][j] = new WorldTransform();
-	//			worldTransformBlocks_[i][j]->Initialize();
-	//			worldTransformBlocks_[i][j]->translation_.x = kBlockWidth * j;
-	//			worldTransformBlocks_[i][j]->translation_.y = kBlockHeight * i;
-	//		} else {
-	//			worldTransformBlocks_[i][j] = nullptr;
-	//		}
-	//	}
-	//}
 
 	// デバッグカメラの生成
 	debugCamera_ = new DebugCamera(1280, 720);
